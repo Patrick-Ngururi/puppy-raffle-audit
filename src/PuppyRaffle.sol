4fa72575ef98b058b305d5d1fe0d7e6aaeb32df1
Patrick-Ngururi <patrick9ngururi8@gmail.com>
2025-09-27 21:59:48 +0300

    Integer overflow exploit

--- src/PuppyRaffle.sol
+++ src/PuppyRaffle.sol
@@ -135,9 +135,14 @@ contract PuppyRaffle is ERC721, Ownable {
         uint256 winnerIndex =
             uint256(keccak256(abi.encodePacked(msg.sender, block.timestamp, block.difficulty))) % players.length;
         address winner = players[winnerIndex];
+        // @Audit: Why the calculation for totalAmountCollected, why not address(this).balance?
         uint256 totalAmountCollected = players.length * entranceFee;
+        // @Audit:80% prizePool, 20% fee. Is this correct? Arithmetic may lead to precision loss
         uint256 prizePool = (totalAmountCollected * 80) / 100;
         uint256 fee = (totalAmountCollected * 20) / 100;
+        // @Audit: Total fees the owner should be able to collect. Why the casting? Overflow.
+        // @audit integer overflow
+        // Fixes: Newer version of solidity, bigger uints
         totalFees = totalFees + uint64(fee);
 
         uint256 tokenId = totalSupply();
